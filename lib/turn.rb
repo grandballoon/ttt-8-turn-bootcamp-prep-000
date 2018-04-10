b166d678091e235b604bc62346794e4934c3ec51
Luke Conley <luke.sovern.conley@gmail.com>
2018-04-10 19:58:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,8 +43,7 @@ user_index = input_to_index(user_move)
 validity = valid_move?(board, user_index)
 if validity == false
   turn(board)
-elsif validity == true
-  move(board, index, token)
+
 end
 
 end
