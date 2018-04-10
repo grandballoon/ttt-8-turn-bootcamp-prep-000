d27232c656a97c719a44652211f71819614e141b
Luke Conley <luke.sovern.conley@gmail.com>
2018-04-10 19:43:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,11 +40,6 @@ def turn (board)
  puts "Please enter 1-9:"
 user_move = gets.strip
 user_index = input_to_index(user_move)
-if valid_move?(board, user_index) == true
-  move
-  puts board 
-elsif valid_move?(board, user_index) == false
-  turn
-end
+
 
 end
